cf6a6a100383297a503c938382bf5ac99ad896f0
anna wang <anna_wang@brown.edu>
2025-10-07 14:22:46 -0400

    feat: exercise 6 simple draft finished

--- exercise6/toggle.js
+++ exercise6/toggle.js
@@ -1,26 +1,64 @@
-const button = document.getElementById("toggle-button");
+// ui
+const body = document.body;
+const nav = document.querySelector("nav");
+const astigButton = document.getElementById("astig-button");
+const toggleButton = document.getElementById("toggle-button");
+const navButton = document.getElementById("nav-button");
+
+// lamp
 const lampGlow = document.getElementById("lamp-glow");
 const lampShade = document.getElementById("lamp-shade");
 const lampNeck = document.getElementById("lamp-neck");
 const lampBase = document.getElementById("lamp-base");
-const body = document.body;
 
 let isLight = true;
+let isAstig = false;
 
 setColor();
 
-button.addEventListener("click", () => {
+toggleButton.addEventListener("click", () => {
   isLight = !isLight;
-  const next = body.classList.contains("theme-on") ? "off" : "on";
+  if (isLight) {
+    isAstig = false;
+    astigButton.textContent = "Astigmatism On";
+  }
   setColor();
 });
 
+astigButton.addEventListener("click", () => {
+  isAstig = !isAstig;
+  setAstig();
+});
+
+function setAstig() {
+  if (!isLight) {
+    if (isAstig) {
+      lampGlow.classList.add("lamp-glow-astig");
+      lampGlow.classList.remove("lamp-glow");
+      astigButton.textContent = "Astigmatism Off";
+    } else {
+      lampGlow.classList.remove("lamp-glow-astig");
+      lampGlow.classList.add("lamp-glow");
+      astigButton.textContent = "Astigmatism On";
+    }
+  }
+}
+
 function setColor() {
   if (isLight) {
-    // color
+    // ui
     body.classList.add("body-light");
     body.classList.remove("body-dark");
-    button.textContent = "Lights Off";
+    navButton.classList.add("nav-button-light");
+    navButton.classList.remove("nav-button-dark");
+    nav.classList.add("nav-light");
+    nav.classList.remove("nav-dark");
+
+    // button
+    astigButton.style.visibility = "hidden";
+    toggleButton.classList.add("toggle-button-light");
+    toggleButton.classList.remove("toggle-button-dark");
+    toggleButton.textContent = "Lights Off";
 
     // lamp
     lampGlow.style.visibility = "hidden";
@@ -31,10 +69,21 @@ function setColor() {
     lampBase.classList.add("lamp-base-light");
     lampBase.classList.remove("lamp-base-dark");
   } else {
-    // color
+    // ui
     body.classList.add("body-dark");
     body.classList.remove("body-light");
-    button.textContent = "Lights On";
+    navButton.classList.add("nav-button-dark");
+    navButton.classList.remove("nav-button-light");
+    nav.classList.add("nav-dark");
+    nav.classList.remove("nav-light");
+
+    // buttons
+    astigButton.style.visibility = "visible";
+    astigButton.classList.add("toggle-button-dark");
+    astigButton.classList.remove("toggle-button-light");
+    toggleButton.classList.add("toggle-button-dark");
+    toggleButton.classList.remove("toggle-button-light");
+    toggleButton.textContent = "Lights On";
 
     //lamp
     lampGlow.style.visibility = "visible";
