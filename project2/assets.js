c50f0ec7e23db088e73e176d166bec312d90c135
anna wang <anna_wang@brown.edu>
2025-12-16 11:06:51 -0500

    feat: added all bird descriptions and images from e-bird

---
+++ project2/assets.js
@@ -0,0 +1,189 @@
+export const timeIcons = {
+  dawn: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+         viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+         class="icon icon-tabler icon-tabler-sunset-2">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M3 13h1" />
+      <path d="M20 13h1" />
+      <path d="M5.6 6.6l.7 .7" />
+      <path d="M18.4 6.6l-.7 .7" />
+      <path d="M8 13a4 4 0 1 1 8 0" />
+      <path d="M3 17h18" />
+      <path d="M7 20h5" />
+      <path d="M16 20h1" />
+      <path d="M12 5v-1" />
+    </svg>
+  `,
+
+  day: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sun"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
+  `,
+
+  dusk: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+         viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+         class="icon icon-tabler icon-tabler-haze-moon">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M3 16h18" />
+      <path d="M3 20h18" />
+      <path d="M8.296 16c-2.268 -1.4 -3.598 -4.087 -3.237 -6.916c.443 -3.48 3.308 -6.083 6.698 -6.084v.006h.296c-1.991 1.916 -2.377 5.03 -.918 7.405c1.459 2.374 4.346 3.33 6.865 2.275a6.888 6.888 0 0 1 -2.777 3.314" />
+    </svg>
+  `,
+
+  night: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+         viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+         class="icon icon-tabler icon-tabler-moon">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+    </svg>
+  `,
+};
+
+export const weatherIcons = {
+  clear: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sun"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
+  `,
+  partly: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sun-wind"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.468 10a4 4 0 1 0 -5.466 5.46" /><path d="M2 12h1" /><path d="M11 3v1" /><path d="M11 20v1" /><path d="M4.6 5.6l.7 .7" /><path d="M17.4 5.6l-.7 .7" /><path d="M5.3 17.7l-.7 .7" /><path d="M15 13h5a2 2 0 1 0 0 -4" /><path d="M12 16h5.714l.253 0a2 2 0 0 1 2.033 2a2 2 0 0 1 -2 2h-.286" /></svg>
+  `,
+  cloudy: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.657 18c-2.572 0 -4.657 -2.007 -4.657 -4.483c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.913 0 3.464 1.56 3.464 3.486c0 1.927 -1.551 3.487 -3.465 3.487h-11.878" /></svg>
+  `,
+  rain: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-rain"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" /><path d="M11 13v2m0 3v2m4 -5v2m0 3v2" /></svg>  `,
+  snow: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-snow"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" /><path d="M11 15v.01m0 3v.01m0 3v.01m4 -4v.01m0 3v.01" /></svg>
+  `,
+  storm: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-bolt"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 18.004h-6.343c-2.572 -.004 -4.657 -2.011 -4.657 -4.487c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.396 0 2.6 .831 3.148 2.03" /><path d="M19 16l-2 3h4l-2 3" /></svg>`,
+};
+
+export const volumeIcons = {
+  on: `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+  stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M15 8a5 5 0 0 1 0 8" />
+  <path d="M17.7 5a9 9 0 0 1 0 14" />
+  <path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5
+    a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" />
+</svg>`,
+  off: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-volume-off"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 1.912 4.934m-1.377 2.602a5 5 0 0 1 -.535 .464" /><path d="M17.7 5a9 9 0 0 1 2.362 11.086m-1.676 2.299a9 9 0 0 1 -.686 .615" /><path d="M9.069 5.054l.431 -.554a.8 .8 0 0 1 1.5 .5v2m0 4v8a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l1.294 -1.664" /><path d="M3 3l18 18" /></svg>`,
+};
+
+export const pond = `         ___________________
+     ___/                   \\__
+  __/             ^^           \\___
+ /                      ^          \\__
+|         ^^^                         \\
+|  ^^                                  | 
+ \\____         ^^            ^^^   ___/
+      \\___           ^     _______/
+          \\_______________/            `;
+
+export const trees = {
+  near: `|  |  |
+| |   |
+|     |
+|     |
+|  |  |
+|     |
+|   | |
+|     |
+|     |
+|     |
+|     |
+|     |
+| |   |
+|  |  |
+|     |
+|   | |
+|     |
+|     |
+| |   |
+|     |
+|     |
+|     |
+|     |
+|     |
+|  |  |
+|   | |
+|     |
+|     |
+|     |
+|     |
+|     |
+|     |
+|     |`,
+  mid: `|    |
+|    |
+|    |
+| |  |
+|  | |
+|    |
+|    |
+|    |
+||   |
+|    |
+|    |
+|    |
+|    |
+|  | |
+|    |
+|   ||
+|    |
+|    |
+| |  |
+|  | |
+|    |
+|    |
+||   |
+|    |
+|    |
+|    |
+|  | |
+|    |
+| |  |
+|   ||
+|    |
+|    |
+|    |`,
+  far: `|   |
+|  ||
+|   |
+|   |
+|   |
+|   |
+| | |
+|   |
+||  |
+|   |
+|   |
+|   |
+|   |
+|   |
+|   |
+| | |
+|   |
+|   |
+|   |
+||  |
+| | |
+|   |
+|   |
+|   |
+|   |
+|   |
+| | |
+|   |
+|  ||
+|   |
+|   |
+|   |
+|   |`,
+};
--- project2/script.js
+++ project2/script.js
@@ -1,229 +1,44 @@
-let birds = [];
-let currentCaller = null;
-let callInterval = null;
-let mode = "realtime";
-let isPaused = false;
+import { timeIcons, weatherIcons, volumeIcons, pond, trees } from "./assets.js";
 
-const timeIcons = {
-  dawn: `
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-         viewBox="0 0 24 24" fill="none" stroke="currentColor"
-         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-         class="icon icon-tabler icon-tabler-sunset-2">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M3 13h1" />
-      <path d="M20 13h1" />
-      <path d="M5.6 6.6l.7 .7" />
-      <path d="M18.4 6.6l-.7 .7" />
-      <path d="M8 13a4 4 0 1 1 8 0" />
-      <path d="M3 17h18" />
-      <path d="M7 20h5" />
-      <path d="M16 20h1" />
-      <path d="M12 5v-1" />
-    </svg>
-  `,
+let birds = []; // stored fetched bird data
+let currentCaller = null; // bird currently making a random call
+let mode = "realtime"; // realtime, dawn, day, dusk, night
+let isPaused = false; // to pause ambient logic when bird modal is open
 
-  day: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sun"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
-  `,
+// handle ambient random bird sounds
+let ambientAudio = null;
+let isMuted = false;
+window.globalMuted = false;
 
-  dusk: `
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-         viewBox="0 0 24 24" fill="none" stroke="currentColor"
-         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-         class="icon icon-tabler icon-tabler-haze-moon">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M3 16h18" />
-      <path d="M3 20h18" />
-      <path d="M8.296 16c-2.268 -1.4 -3.598 -4.087 -3.237 -6.916c.443 -3.48 3.308 -6.083 6.698 -6.084v.006h.296c-1.991 1.916 -2.377 5.03 -.918 7.405c1.459 2.374 4.346 3.33 6.865 2.275a6.888 6.888 0 0 1 -2.777 3.314" />
-    </svg>
-  `,
-
-  night: `
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-         viewBox="0 0 24 24" fill="none" stroke="currentColor"
-         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-         class="icon icon-tabler icon-tabler-moon">
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-      <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-    </svg>
-  `,
-};
-
-const weatherIcons = {
-  clear: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sun"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>
-  `,
-  partly: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-sun-wind"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.468 10a4 4 0 1 0 -5.466 5.46" /><path d="M2 12h1" /><path d="M11 3v1" /><path d="M11 20v1" /><path d="M4.6 5.6l.7 .7" /><path d="M17.4 5.6l-.7 .7" /><path d="M5.3 17.7l-.7 .7" /><path d="M15 13h5a2 2 0 1 0 0 -4" /><path d="M12 16h5.714l.253 0a2 2 0 0 1 2.033 2a2 2 0 0 1 -2 2h-.286" /></svg>
-  `,
-  cloudy: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6.657 18c-2.572 0 -4.657 -2.007 -4.657 -4.483c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.913 0 3.464 1.56 3.464 3.486c0 1.927 -1.551 3.487 -3.465 3.487h-11.878" /></svg>
-  `,
-  rain: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-rain"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" /><path d="M11 13v2m0 3v2m4 -5v2m0 3v2" /></svg>  `,
-  snow: `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-snow"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 18a4.6 4.4 0 0 1 0 -9a5 4.5 0 0 1 11 2h1a3.5 3.5 0 0 1 0 7" /><path d="M11 15v.01m0 3v.01m0 3v.01m4 -4v.01m0 3v.01" /></svg>
-  `,
-  storm: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-cloud-bolt"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 18.004h-6.343c-2.572 -.004 -4.657 -2.011 -4.657 -4.487c0 -2.475 2.085 -4.482 4.657 -4.482c.393 -1.762 1.794 -3.2 3.675 -3.773c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.396 0 2.6 .831 3.148 2.03" /><path d="M19 16l-2 3h4l-2 3" /></svg>`,
-};
-
-const iconVolume = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-  stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M15 8a5 5 0 0 1 0 8" />
-  <path d="M17.7 5a9 9 0 0 1 0 14" />
-  <path d="M6 15h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l3.5 -4.5
-    a.8 .8 0 0 1 1.5 .5v14a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5" />
-</svg>`;
-
-const iconVolumeOff = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-volume-off"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 8a5 5 0 0 1 1.912 4.934m-1.377 2.602a5 5 0 0 1 -.535 .464" /><path d="M17.7 5a9 9 0 0 1 2.362 11.086m-1.676 2.299a9 9 0 0 1 -.686 .615" /><path d="M9.069 5.054l.431 -.554a.8 .8 0 0 1 1.5 .5v2m0 4v8a.8 .8 0 0 1 -1.5 .5l-3.5 -4.5h-2a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h2l1.294 -1.664" /><path d="M3 3l18 18" /></svg>`;
-
-const pond = `         ___________________
-     ___/                   \\__
-  __/             ^^           \\___
- /                      ^          \\__
-|         ^^^                         \\
-|  ^^                                  | 
- \\____         ^^            ^^^   ___/
-      \\___           ^     _______/
-          \\_______________/            `;
-
-const trees = {
-  near: `|  |  |
-| |   |
-|     |
-|     |
-|  |  |
-|     |
-|   | |
-|     |
-|     |
-|     |
-|     |
-|     |
-| |   |
-|  |  |
-|     |
-|   | |
-|     |
-|     |
-| |   |
-|     |
-|     |
-|     |
-|     |
-|     |
-|  |  |
-|   | |
-|     |
-|     |
-|     |
-|     |
-|     |
-|     |
-|     |`,
-  mid: `|    |
-|    |
-|    |
-| |  |
-|  | |
-|    |
-|    |
-|    |
-||   |
-|    |
-|    |
-|    |
-|    |
-|  | |
-|    |
-|   ||
-|    |
-|    |
-| |  |
-|  | |
-|    |
-|    |
-||   |
-|    |
-|    |
-|    |
-|  | |
-|    |
-| |  |
-|   ||
-|    |
-|    |
-|    |`,
-  far: `|   |
-|  ||
-|   |
-|   |
-|   |
-|   |
-| | |
-|   |
-||  |
-|   |
-|   |
-|   |
-|   |
-|   |
-|   |
-| | |
-|   |
-|   |
-|   |
-||  |
-| | |
-|   |
-|   |
-|   |
-|   |
-|   |
-| | |
-|   |
-|  ||
-|   |
-|   |
-|   |
-|   |`,
-};
-
-const PERCH_RULES = {
-  ground: ["duck", "swan", "robin", "mourningdove"],
-  rightSide: ["downywoodpecker", "redbellied"], // woodpeckers
-};
-
+// fires when the HTML document has been completely parsed,
+// and all deferred scripts (<script defer src="…"> and <script type="module">) have downloaded and executed
 window.addEventListener("DOMContentLoaded", () => {
-  // const ascii =
-  //   " 　_---\n -´0 \\\n  l ( \\\\.____\n   \\_`_.----\n===/=/========";
-
-  // const el = document.createElement("pre");
-  // el.textContent = ascii;
-  // document.body.appendChild(el);
+  // set up the scene
   applyBackground();
+
+  // update time display every second
   updateTimePanel();
   setInterval(updateTimePanel, 1000);
 
+  // update weather/temp display for prov every 10 mins
   updateWeather();
-  setInterval(updateWeather, 10 * 60 * 1000); // every 10 mins
+  setInterval(updateWeather, 10 * 60 * 1000);
 
   placeAscii();
 
-  let isMuted = false;
   const soundButton = document.getElementById("sound-button");
-  soundButton.innerHTML = iconVolume;
+  soundButton.innerHTML = volumeIcons.on;
 
   soundButton.addEventListener("click", () => {
     isMuted = !isMuted;
-
-    soundButton.innerHTML = isMuted ? iconVolumeOff : iconVolume;
-
     window.globalMuted = isMuted;
+
+    // set volume true or false according to whether user has muted sound
+    if (ambientAudio) {
+      ambientAudio.volume = isMuted ? 0 : 0.6;
+    }
+
+    soundButton.innerHTML = isMuted ? volumeIcons.off : volumeIcons.on;
   });
 
   const modeButton = document.getElementById("mode-button");
@@ -285,28 +100,6 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-const TREE_ANCHORS = {
-  near: [{ el: "trees-near", dx: 8, dy: 20 }],
-  mid: [
-    { el: "trees-mid-1", dx: 8, dy: 20 },
-    { el: "trees-mid-2", dx: 8, dy: 20 },
-  ],
-  far: [
-    { el: "trees-far-1", dx: 8, dy: 20 },
-    { el: "trees-far-2", dx: 8, dy: 20 },
-  ],
-  ground: {
-    minY: window.innerHeight * 0.7,
-    maxY: window.innerHeight * 0.88,
-  },
-};
-
-function getBirdsForCurrentTime() {
-  const cur = getCurrentMode();
-
-  return birds.filter((b) => b.activeTimes.includes(cur));
-}
-
 function startCallLoop() {
   // small delay to ensure birds are placed first
   setTimeout(() => {
@@ -342,16 +135,23 @@ function pickRandomBirdCall() {
 
   console.log("Playing:", randomBird.id);
 
-  const audio = new Audio(randomBird.sound);
-  audio.type = "audio/wav";
+  if (window.globalMuted) {
+    if (birdDiv) birdDiv.classList.remove("calling");
+    scheduleNextCall();
+    return;
+  }
+
+  ambientAudio = new Audio(randomBird.sound);
+  ambientAudio.volume = window.globalMuted ? 0 : 1;
 
   // play audio
-  audio.play().catch((err) => {
+  ambientAudio.play().catch((err) => {
     console.warn("Audio failed to play:", err);
     scheduleNextCall();
   });
 
-  audio.onended = () => {
+  ambientAudio.onended = () => {
+    ambientAudio = null;
     if (birdDiv) birdDiv.classList.remove("calling");
     scheduleNextCall();
   };
@@ -386,16 +186,6 @@ function enableClicks() {
     });
   });
 }
-
-// function updateInfoPanel(bird) {
-//   const panel = document.getElementById("bird-info");
-//   if (!panel) return;
-
-//   panel.innerHTML = `
-//     <h2>${bird.name}</h2>
-//     <p>${bird.description}</p>
-//   `;
-// }
 
 // for ui and background
 function getDayState(date = new Date()) {
@@ -778,6 +568,8 @@ let modalAudio = null;
 
 document.getElementById("modal-sound-btn").addEventListener("click", () => {
   if (!modalAudio) return;
+  if (!modalAudio) return;
+  if (window.globalMuted) return;
 
   modalAudio.currentTime = 0;
   modalAudio.play().catch((err) => console.warn("Sound failed:", err));
@@ -802,7 +594,6 @@ function openBirdModal(bird) {
   document.getElementById("modal-description").textContent =
     bird.description || "";
   modalAudio = new Audio(bird.sound);
-  modalAudio.type = "audio/wav";
 
   overlay.style.display = "block";
 }
